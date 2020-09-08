7e2b28d289a6174bee8950d9e367e1fb85ec5086
Mohammad Denny <mohammaddenny94@gmail.com>
2020-09-08 11:29:04 +0700

    Updates

---
+++ precache-manifest.39650370c78e2596dfb2d2242e60d92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7587b68668e2b3bb6ede405d7e29a138",
+    "url": "/denode/index.html"
+  },
+  {
+    "revision": "e040ae72abc1cb471153",
+    "url": "/denode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a70d12f7f41a69286cd0",
+    "url": "/denode/static/js/2.84d1bae4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/denode/static/js/2.84d1bae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e040ae72abc1cb471153",
+    "url": "/denode/static/js/main.ae44d09a.chunk.js"
+  },
+  {
+    "revision": "e0739c95fb1e5d95274e",
+    "url": "/denode/static/js/runtime-main.34b0e449.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/denode/static/media/logo.5d5d9eef.svg"
+  }
+]);
